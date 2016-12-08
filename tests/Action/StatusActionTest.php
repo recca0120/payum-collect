5cc2dab617d8d0995a4bfe4eb51b11c0494a70e0
Recca Tsai <recca0120.gmail.com>
2016-12-08 14:51:07 +0800

    update tests

--- tests/Action/StatusActionTest.php
+++ tests/Action/StatusActionTest.php
@@ -13,274 +13,95 @@ class StatusActionTest extends PHPUnit_Framework_TestCase
 
     public function test_mark_new()
     {
-        /*
-        |------------------------------------------------------------
-        | Arrange
-        |------------------------------------------------------------
-        */
-
-        $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject();
-
-        /*
-        |------------------------------------------------------------
-        | Act
-        |------------------------------------------------------------
-        */
-
-        $request->shouldReceive('getModel')->andReturn($details);
-
-        $action = new StatusAction();
-        $action->execute($request);
-
-        /*
-        |------------------------------------------------------------
-        | Assert
-        |------------------------------------------------------------
-        */
-
-        $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived('markNew')->once();
+        $this->validate([], 'markNew');
     }
 
     public function test_mark_captured_when_ret_is_ok()
     {
-        /*
-        |------------------------------------------------------------
-        | Arrange
-        |------------------------------------------------------------
-        */
-
-        $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject([
-            'ret' => 'OK',
-            'cust_order_no' => '20120403000003',
-            'order_amount' => '12345',
-            'send_time' => '2013-04-03 07:17:25',
-            'acquire_time' => '2013-04-03 07:19:32',
-            'auth_code' => '851425',
-            'card_no' => '0085',
-            'notify_time' => '2013-04-03 07:19:46',
-            'chk' => 'a1eeb49d7a559393d05f5bbd81fbba84',
-        ]);
-
-        /*
-        |------------------------------------------------------------
-        | Act
-        |------------------------------------------------------------
-        */
-
-        $request->shouldReceive('getModel')->andReturn($details);
-
-        $action = new StatusAction();
-        $action->execute($request);
-
-        /*
-        |------------------------------------------------------------
-        | Assert
-        |------------------------------------------------------------
-        */
-
-        $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived('markCaptured')->once();
+        $this->validate(['ret' => 'OK'], 'markCaptured');
     }
 
     public function test_mark_failed_when_ret_is_fail()
     {
-        /*
-        |------------------------------------------------------------
-        | Arrange
-        |------------------------------------------------------------
-        */
-
-        $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject([
-            'ret' => 'FAIL',
-            'cust_order_no' => '20120403000003',
-            'order_amount' => '12345',
-            'send_time' => '2013-04-03 07:17:25',
-            'notify_time' => '2013-04-03 07:19:46',
-            'chk' => 'a1eeb49d7a559393d05f5bbd81fbba84',
-        ]);
-
-        /*
-        |------------------------------------------------------------
-        | Act
-        |------------------------------------------------------------
-        */
-
-        $request->shouldReceive('getModel')->andReturn($details);
-
-        $action = new StatusAction();
-        $action->execute($request);
-
-        /*
-        |------------------------------------------------------------
-        | Assert
-        |------------------------------------------------------------
-        */
-
-        $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived('markFailed')->once();
+        $this->validate(['ret' => 'FAIL'], 'markFailed');
     }
 
     public function test_mark_canceled_when_status_is_ok()
     {
-        /*
-        |------------------------------------------------------------
-        | Arrange
-        |------------------------------------------------------------
-        */
-
-        $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject([
+        $this->validate([
             'status' => 'OK',
             'cust_order_no' => '20120403001276',
-        ]);
-
-        /*
-        |------------------------------------------------------------
-        | Act
-        |------------------------------------------------------------
-        */
-
-        $request->shouldReceive('getModel')->andReturn($details);
-
-        $action = new StatusAction();
-        $action->execute($request);
-
-        /*
-        |------------------------------------------------------------
-        | Assert
-        |------------------------------------------------------------
-        */
-
-        $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived('markCanceled')->once();
+        ], 'markCanceled');
     }
 
     public function test_mark_refunded_when_status_is_ok()
     {
-        /*
-        |------------------------------------------------------------
-        | Arrange
-        |------------------------------------------------------------
-        */
-
-        $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject([
+        $this->validate([
             'status' => 'OK',
             'cust_order_no' => '20120403001276',
             'refund_amount' => 12000,
-        ]);
-
-        /*
-        |------------------------------------------------------------
-        | Act
-        |------------------------------------------------------------
-        */
-
-        $request->shouldReceive('getModel')->andReturn($details);
-
-        $action = new StatusAction();
-        $action->execute($request);
-
-        /*
-        |------------------------------------------------------------
-        | Assert
-        |------------------------------------------------------------
-        */
-
-        $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived('markRefunded')->once();
+        ], 'markRefunded');
     }
 
     public function test_mark_failed_when_status_is_error()
     {
-        /*
-        |------------------------------------------------------------
-        | Arrange
-        |------------------------------------------------------------
-        */
-
-        $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject([
+        $this->validate([
             'status' => 'ERROR',
             'send_time' => '異常',
-        ]);
-
-        /*
-        |------------------------------------------------------------
-        | Act
-        |------------------------------------------------------------
-        */
-
-        $request->shouldReceive('getModel')->andReturn($details);
-
-        $action = new StatusAction();
-        $action->execute($request);
-
-        /*
-        |------------------------------------------------------------
-        | Assert
-        |------------------------------------------------------------
-        */
-
-        $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived('markFailed')->once();
+        ], 'markFailed');
     }
 
     public function test_apn_status_is_b()
     {
-        $this->validateApnStatus('B', 'markCaptured');
+        $this->validate(['status' => 'B'], 'markCaptured');
     }
 
     public function test_apn_status_is_o()
     {
-        $this->validateApnStatus('O', 'markSuspended');
+        $this->validate(['status' => 'O'], 'markSuspended');
     }
 
     public function test_apn_status_is_e()
     {
-        $this->validateApnStatus('E', 'markCaptured');
+        $this->validate(['status' => 'E'], 'markCaptured');
     }
 
     public function test_apn_status_is_f()
     {
-        $this->validateApnStatus('F', 'markFailed');
+        $this->validate(['status' => 'F'], 'markFailed');
     }
 
     public function test_apn_status_is_d()
     {
-        $this->validateApnStatus('D', 'markExpired');
+        $this->validate(['status' => 'D'], 'markExpired');
     }
 
     public function test_apn_status_is_p()
     {
-        $this->validateApnStatus('P', 'markCaptured');
+        $this->validate(['status' => 'P'], 'markCaptured');
     }
 
     public function test_apn_status_is_m()
     {
-        $this->validateApnStatus('M', 'markCanceled');
+        $this->validate(['status' => 'M'], 'markCanceled');
     }
 
     public function test_apn_status_is_n()
     {
-        $this->validateApnStatus('N', 'markFailed');
+        $this->validate(['status' => 'N'], 'markFailed');
     }
 
     public function test_apn_status_is_q()
     {
-        $this->validateApnStatus('Q', 'markRefunded');
+        $this->validate(['status' => 'Q'], 'markRefunded');
     }
 
     public function test_apn_status_is_r()
     {
-        $this->validateApnStatus('R', 'markFailed');
+        $this->validate(['status' => 'R'], 'markFailed');
     }
 
-    public function validateApnStatus($status, $marked)
+    protected function validate($input, $type)
     {
         /*
         |------------------------------------------------------------
@@ -289,24 +110,7 @@ class StatusActionTest extends PHPUnit_Framework_TestCase
         */
 
         $request = m::spy('Payum\Core\Request\GetStatusInterface');
-        $details = new ArrayObject([
-            'api_id' => 'CC0000000001',
-            'trans_id' => '550e8400e29b41d4a716446655440000',
-            'order_no' => 'PO5488277',
-            'amount' => 1250,
-            'status' => $status,
-            'payment_code' => 1,
-            'payment_detail' => [
-                'auth_code' => '123456',
-                'auth_card_no' => '0000',
-            ],
-            'memo' => [],
-            'expire_time' => '2013-09-28T08:15:00+08:00',
-            'create_time' => '2013-09-28T08:00:00+08:00',
-            'modify_time' => '2013-09-28T08:30:00+08:00',
-            'nonce' => '1234569999',
-            'checksum' => '1d1e6c42757166243312b2ad05a5dda8',
-        ]);
+        $details = new ArrayObject($input);
 
         /*
         |------------------------------------------------------------
@@ -326,6 +130,6 @@ class StatusActionTest extends PHPUnit_Framework_TestCase
         */
 
         $request->shouldHaveReceived('getModel')->twice();
-        $request->shouldHaveReceived($marked)->once();
+        $request->shouldHaveReceived($type)->once();
     }
 }
